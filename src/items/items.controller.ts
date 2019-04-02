4da9c520caebe738b8772b95a183976e7061d249
Tyrese Williams <tyresewilliams@Tyreses-MacBook-Pro.local>
2019-04-01 22:55:19 -0400

    controller methods

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,16 +1,30 @@
-import { Get, Post, Put, Delete, Body, Controller } from '@nestjs/common';
+import { Param, Req, Get, Post, Put, Delete, Body, Controller, Res } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
-
+import { Request, Response } from 'express';
 @Controller('items')
 
 export class ItemsController {
     @Get() 
     findAll(): string {
-        return 'Get all items';
+        return 'GET ALL ITEMS'
     }
 
     @Post() 
     create(@Body() createItemDto: CreateItemDto): string {
         return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
     }
+    @Get(':id')
+    findOne(@Param('id') id): string {
+        return `Item ${id}`;
+    }
+    @Delete(':id') 
+        delete(@Param('id'), id): string {
+            return `Delete ${id}`;
+        }
+        @Put(':id')
+        update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+            return `Update ${id} - Name: ${updateItemDto.name}`
+        }
+    }
 }
+
